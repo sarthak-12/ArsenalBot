adb09a198ab590b7c452884f2fab34d1e9b9d569
Sarthak Khanna <42035527+sarthak-12@users.noreply.github.com>
2020-07-09 18:54:57 +0530

    Add files via upload

---
+++ ArsenalBot.py
@@ -0,0 +1,17 @@
+import tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token_key, access_token_key_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+search_key = 'OnePlus' #keywords to be searched
+num = 5  #number of tweets to be searched
+for tweet in tweepy.Cursor(api.search , search_key).items(num):
+	try:
+		tweet.favorite()
+		print('Liked that tweet.')
+	except tweepy.TweepError as e:
+		print(e.reason)
+	except StopIteration:
+		break
